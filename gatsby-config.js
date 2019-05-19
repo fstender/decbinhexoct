d9db188c92da9339992aafdbf69744960fe85cec
Frank Stender <frank.stender@hc-vision.de>
2019-05-19 21:56:15 +0200

    Removed unnecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,19 @@
 module.exports = {
-  pathPrefix: `/gatsby-starter-hyperspace/`, // This path is subpath of your hosting https://domain/portfolio
   siteMetadata: {
-    title: 'Gatsby Starter Hyperspace',
+    title: 'DecBinHexOct',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Hyperspace',
-        short_name: 'starter',
+        name: 'decbinhexoct',
+        short_name: 'decbinhexoct',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'standalone',
-        icon: 'src/assets/img/website-icon.png', // This path is relative to the root of the site.
+        // icon: 'src/assets/img/website-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
